efbdd89c2bed411df9cedb75549030dec8a1b5c5
maiyama <myam0118@gmail.com>
2022-03-15 08:55:22 +0900

    統一性のために検索画面もコードで生成するようにする

--- iOSEngineerCodeCheck/SceneDelegate.swift
+++ iOSEngineerCodeCheck/SceneDelegate.swift
@@ -16,7 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         _ scene: UIScene, willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = UIStoryboard(name: "RepositorySearch", bundle: nil)
+            .instantiateInitialViewController()
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {}
