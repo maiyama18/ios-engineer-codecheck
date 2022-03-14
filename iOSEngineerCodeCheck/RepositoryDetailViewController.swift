093e9594cb9f60b0244d6e85c65fdf38b8e553a4
maiyama <myam0118@gmail.com>
2022-03-14 21:30:48 +0900

    レポジトリ詳細の画像を表示するメソッドの命名と役割が一致するようにリファクタリングする

--- iOSEngineerCodeCheck/RepositoryDetailViewController.swift
+++ iOSEngineerCodeCheck/RepositoryDetailViewController.swift
@@ -28,19 +28,16 @@ class RepositoryDetailViewController: UIViewController {
 
         let repository = searchVC.repositories[searchVC.selectedIndex]
 
+        titleLabel.text = repository["full_name"] as? String
         languageLabel.text = "Written in \(repository["language"] as? String ?? "")"
         starsLabel.text = "\(repository["stargazers_count"] as? Int ?? 0) stars"
         watchersLabel.text = "\(repository["wachers_count"] as? Int ?? 0) watchers"
         forksLabel.text = "\(repository["forks_count"] as? Int ?? 0) forks"
         openIssuesLabel.text = "\(repository["open_issues_count"] as? Int ?? 0) open issues"
-        getImage()
+        setupAvatarImage(repository: repository)
     }
 
-    func getImage() {
-        let repository = searchVC.repositories[searchVC.selectedIndex]
-
-        titleLabel.text = repository["full_name"] as? String
-
+    func setupAvatarImage(repository: [String: Any]) {
         if let owner = repository["owner"] as? [String: Any] {
             if let avatarURL = owner["avatar_url"] as? String {
                 URLSession.shared.dataTask(with: URL(string: avatarURL)!) { (data, res, err) in
