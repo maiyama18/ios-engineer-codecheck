ddc3291b75099d3c557c9218247e055e5d508de2
maiyama <myam0118@gmail.com>
2022-03-16 21:25:32 +0900

    ViewController に SwiftUI.View を表示するためのメソッドを追加する

---
+++ iOSEngineerCodeCheck/Extensions/UIView+Extension.swift
@@ -0,0 +1,25 @@
+//
+//  UIView+Extension.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by maiyama on 2022/03/16.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func pinEdgesToSuperView() {
+        guard let view = superview else {
+            return
+        }
+
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: view.topAnchor),
+            leftAnchor.constraint(equalTo: view.leftAnchor),
+            rightAnchor.constraint(equalTo: view.rightAnchor),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
---
+++ iOSEngineerCodeCheck/Extensions/UIViewController+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Extension.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by maiyama on 2022/03/16.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import SwiftUI
+
+extension UIViewController {
+    public func hostSwiftUIView<Content: View>(_ rootView: Content) {
+        let hostingVC = UIHostingController(rootView: rootView)
+        addChild(hostingVC)
+        hostingVC.didMove(toParent: self)
+        view.addSubview(hostingVC.view)
+        hostingVC.view.translatesAutoresizingMaskIntoConstraints = false
+        hostingVC.view.pinEdgesToSuperView()
+    }
+}
