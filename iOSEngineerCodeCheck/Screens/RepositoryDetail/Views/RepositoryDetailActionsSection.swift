1d05237855f2346a84bf88be6a3c632666722a7e
maiyama <myam0118@gmail.com>
2022-03-18 22:54:35 +0900

    詳細画面でのアクション一覧を表示するセクションを追加する

---
+++ iOSEngineerCodeCheck/Screens/RepositoryDetail/Views/RepositoryDetailActionsSection.swift
@@ -0,0 +1,21 @@
+//
+//  RepositoryDetailActionsSection.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by maiyama on 2022/03/18.
+//  Copyright © 2022 YUMEMI Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct RepositoryDetailActionsSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RepositoryDetailActionsSection_Previews: PreviewProvider {
+    static var previews: some View {
+        RepositoryDetailActionsSection()
+    }
+}
